73a56cb63359c28c04eba1eff0189dd0b68a2adb
FelipeAxel <axelnovais96@gmail.com>
2023-01-18 20:59:46 -0300

    Project create

---
+++ projeto1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import NavBar from './components/Navbar/Navbar';
+function App() {
+  return (
+    <div className="App">
+      <h1>Juro solenemente não fazer nada de bom!</h1>
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
